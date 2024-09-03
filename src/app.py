93d29d8198b60e304eb5d57fe89eeea40baf43fa
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-09-03 16:36:45 +0000

    avanze

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character,Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,64 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_users():
+    try:
+        query_results = User.query.all()
+        print(query_results)
+        results=list(map(lambda item:item.serialize(),query_results))
+        print(results)
+        if results:
+            response_body = {
+                "msg": "ok ",
+                "results": results
+            }
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+            return jsonify(response_body), 200
+        return jsonify({"msg": "users not found"}),404
+    except Exception as e:
+        return jsonify({'error': 'Internal server error','message':str(e)}),500
+    
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    try:
+        #consultar al modelo todos los registros
+        query_results = Character.query.all()
+        print(query_results) #[<Character 1>, <Character 2>] esta informacion es un array []tenemos que transformarla en una informacion que podamos manejar por eso vamos recorrerlo y darle formato con nuestra funcion serialize que se encarga de retornar un objeto legible que yo pueda trabajar
+        results= list(map(lambda item: item.serialize(),query_results)) # en el map tenemos que enviarle 2 valores, el primero la funcion lambda con  el parametro item , entonces cada vez que te posiciones sobre ese valor que se esta consultando aplicale el metodo serialize(), y el segundo valor es el nombre del array que se quiere que recorra
+        print(results)#<map object at 0x749858a16ef0> nos arroja este valor en consola , tenemos que castear en la linea 62 "list(map())" y luego se obtendria [{'id': 1, 'name': 'Luke'}, {'id': 2, 'name': 'Leia'}]
+        
+        if results:
+            response_body = {
+                "msg": "ok",
+                "results": results
+            }
+            return jsonify(response_body), 200
+        return jsonify({"msg": "characters not found"}),404
+    except Exception as e:
+        return jsonify({'error': 'Internal server error','message':str(e)}),500
 
-    return jsonify(response_body), 200
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    query_results = Planet.query.all()
+    print(query_results)
+
+    # try:
+    #     query_results = Planet.query.all()
+    #     results = list(map(lambda item: item.serialize(), query_results))
+        
+    #     if results:
+    #         response_body = {
+    #             "msg": "ok",
+    #             "results": results
+    #         }
+    #         return jsonify(response_body), 200
+    #     return jsonify({"msg": "Character not found"}), 404
+
+    # except Exception as e:
+    #     return jsonify({"error": "Internal server error", "message": str(e)}), 500
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,101 @@
+
 from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name =db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # favorite_character = db.relationship("Favorite_Character",backref ="user", lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "email":self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    # height =db.Column(db.Integer, nullable=False)
+    # mass =db.Column(db.Integer, nullable=False)
+    # hair_color =db.Column(db.String(50), nullable=False)
+    # skin_color =db.Column(db.String(50), nullable=False)
+    # eye_color =db.Column(db.String(50), nullable=False)
+    # gender =db.Column(db.String(50), nullable=False)
+    # favorite_character =db.relationship("Favorite_Character", backref = "character", lazy=True)
+
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # "height": self.height,
+            # "hair_color": self.hair_color,
+            # "skin_color": self.skin_color,
+            # "eye_color": self.eye_color,
+            # "gender": self.gender,
+        }
+
+            # do not serialize the password, its a security breach
+        
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+class Favorite_Character(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    character_id =db.Column(db.Integer, db.ForeignKey("character.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
+    def __repr__(self):
+        return '<Favorite_Character %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_id": self.character_id,
+            "user_id": self.user_id
+        }
+
+
